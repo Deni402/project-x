b80b96710208a6c9f83166f54b6fe431ff6d4960
Denisa <tdenisamihaela@yahoo.com>
2020-07-23 19:49:35 +0300

    update

---
+++ app.js
@@ -0,0 +1,46 @@
+let running = true
+
+while (running) {
+  const op = prompt('Enter the operator')
+
+  switch (op) {
+    case '+': {
+      const x = parseFloat(prompt('Enter the first number'))
+      const y = parseFloat(prompt('Enter the second number'))
+      console.log(x + y)
+      break
+    }
+    case '-': {
+      const x = parseFloat(prompt('Enter the first number'))
+      const y = parseFloat(prompt('Enter the second number'))
+      console.log(x - y)
+      break
+    }
+    case '*': {
+      const x = parseFloat(prompt('Enter the first number'))
+      const y = parseFloat(prompt('Enter the second number'))
+      console.log(x * y)
+      break
+    }
+    case '/': {
+      const x = parseFloat(prompt('Enter the first number'))
+      const y = parseFloat(prompt('Enter the second number'))
+      console.log(x / y)
+      break
+    }
+    case 'power': {
+      const x = parseFloat(prompt('Enter the first number'))
+      const y = parseFloat(prompt('Enter the second number'))
+      console.log(Math.pow(x, y))
+      break
+    }
+    case 'sqrt': {
+      const x = parseFloat(prompt('Enter the first number'))
+      const y = parseFloat(prompt('Enter the second number'))
+      console.log(Math.sqrt(x, y))
+      break
+    }
+  }
+
+  running = confirm('Do you want to keep going?')
+}
