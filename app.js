8ad0877c7b3bda47d791ee811aedc6b5209492d6
Denisa <tdenisamihaela@yahoo.com>
2020-07-28 15:39:54 +0300

    update

--- app.js
+++ app.js
@@ -1,47 +1,89 @@
-let running = true
 
-while (running) {
-  const op = prompt('Enter the operator')
-
-  switch (op) {
-    case '+': {
-      const x = parseFloat(prompt('Enter the first number'))
-      const y = parseFloat(prompt('Enter the second number'))
-      console.log(x + y)
+function getNumber(num) {
+  var inputVar = document.getElementById('input')
+  switch (num){
+    case 1:
+      inputVar.value += '1'
       break
-    }
-    case '-': {
-      const x = parseFloat(prompt('Enter the first number'))
-      const y = parseFloat(prompt('Enter the second number'))
-      console.log(x - y)
+    case 2:
+      inputVar.value += '2'
       break
-    }
-    case '*': {
-      const x = parseFloat(prompt('Enter the first number'))
-      const y = parseFloat(prompt('Enter the second number'))
-      console.log(x * y)
+    case 3:
+      inputVar.value += '3'
       break
-    }
-    case '/': {
-      const x = parseFloat(prompt('Enter the first number'))
-      const y = parseFloat(prompt('Enter the second number'))
-      console.log(x / y)
+    case 4:
+      inputVar.value += '4'
       break
-    }
-    case 'power': {
-      const x = parseFloat(prompt('Enter the first number'))
-      const y = parseFloat(prompt('Enter the second number'))
-      console.log(Math.pow(x, y))
+    case 5:
+      inputVar.value += '5'
       break
-    }
-    case 'sqrt': {
-      const x = parseFloat(prompt('Enter the first number'))
-      const y = parseFloat(prompt('Enter the second number'))
-      console.log(Math.sqrt(x, y))
+    case 6:
+      inputVar.value += '6'
       break
-    }
+    case 7:
+      inputVar.value += '7'
+      break
+    case 8:
+      inputVar.value += '8'
+      break
+    case 9:
+      inputVar.value += '9'
+      break
+    case 0:
+      inputVar.value += '0'
+      break
   }
+}
+
+
+
+
+//   THE BASIC IDEEA FOR CALCULATOR //
+
+// let running = true
+
+// while (running) {
+//   const op = prompt('Enter the operator')
+
+//   switch (op) {
+//     case '+': {
+//       const x = parseFloat(prompt('Enter the first number'))
+//       const y = parseFloat(prompt('Enter the second number'))
+//       console.log(x + y)
+//       break
+//     }
+//     case '-': {
+//       const x = parseFloat(prompt('Enter the first number'))
+//       const y = parseFloat(prompt('Enter the second number'))
+//       console.log(x - y)
+//       break
+//     }
+//     case '*': {
+//       const x = parseFloat(prompt('Enter the first number'))
+//       const y = parseFloat(prompt('Enter the second number'))
+//       console.log(x * y)
+//       break
+//     }
+//     case '/': {
+//       const x = parseFloat(prompt('Enter the first number'))
+//       const y = parseFloat(prompt('Enter the second number'))
+//       console.log(x / y)
+//       break
+//     }
+//     case 'power': {
+//       const x = parseFloat(prompt('Enter the first number'))
+//       const y = parseFloat(prompt('Enter the second number'))
+//       console.log(Math.pow(x, y))
+//       break
+//     }
+//     case 'sqrt': {
+//       const x = parseFloat(prompt('Enter the first number'))
+//       const y = parseFloat(prompt('Enter the second number'))
+//       console.log(Math.sqrt(x, y))
+//       break
+//     }
+//   }
 
   
-  running = confirm('Do you want to keep going?')
-}
+//   running = confirm('Do you want to keep going?')
+// }
